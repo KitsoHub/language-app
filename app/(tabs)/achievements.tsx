46b18021cf652d445470223418878e853a828d61
samKenpachi011 <samuelkabelo1@gmail.com>
2025-03-20 15:43:54 +0200

    ADD: achievement card

--- app/(tabs)/achievements.tsx
+++ app/(tabs)/achievements.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AchievementCard from '@/components/shared/AchievementCard'
+import { achievements } from '@/mocks/achievements'
+
 
 export default function AchievementsPage() {
   return (
     <View>
-      <Text>AchievementsPage</Text>
+      {achievements.map((item)=>(
+        <AchievementCard key={item.id} achievement={item}/>
+
+      ))}
     </View>
   )
 }
