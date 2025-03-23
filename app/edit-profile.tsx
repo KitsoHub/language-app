2116826168b0917ffb38a4a922d3fb8fc94abe6d
samKenpachi011 <samuelkabelo1@gmail.com>
2025-03-23 17:27:23 +0200

    ADD: added inputs

--- app/edit-profile.tsx
+++ app/edit-profile.tsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Stack, useRouter } from 'expo-router'
 import { useAuthStore } from '@/store/auth-store';
+import { Input } from '@/components/ui/Input';
+import { Mail, User } from 'lucide-react-native';
+import { colors } from '@/utils/constants/colors';
+import { Button } from '@/components/ui/Button';
 
 export default function EditProfile() {
     const router = useRouter();
@@ -20,7 +24,9 @@ export default function EditProfile() {
         email:'',
     })
 
-
+    const handleSave = () =>{
+        console.log('Handling Save')
+    }
 
   return (
 <>
@@ -37,6 +43,15 @@ behavior={Platform.OS === 'ios' ?"padding":"height"}
         {/* list of avatars */}
 
         {/* inputs */}
+        <View>
+            <Input label="Full Name" placeholder=' Enter your full name' value={name} onChangeText={setName} error={errors.name} leftIcon={<User size={20} color={colors.gray500} />}/>
+            <Input keyboardType='email-address' autoCapitalize="words" label="Email Address" placeholder='Enter your email address' value={email} onChangeText={setEmail} error={errors.email} leftIcon={<Mail size={20} color={colors.gray500} />}/>
+
+        <Button title='Save Changes'
+        onPress={handleSave}
+        isLoading={isLoading}
+        />
+        </View>
 
 
     </ScrollView>
--- types/index.ts
+++ types/index.ts
@@ -8,6 +8,7 @@ export interface User{
     xp:number;
     level:number;
     joinedAt:string;
+
 }
 
 export interface Language{
