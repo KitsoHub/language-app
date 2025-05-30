2d9e5a98206c76b0e8f17ebf1da9ecda29d704c8
samKenpachi011 <samuelkabelo1@gmail.com>
2025-05-30 13:54:02 +0200

    FIX: refactored debounce on achivement list

--- components/shared/AchivementList.tsx
+++ components/shared/AchivementList.tsx
@@ -1,5 +1,5 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useCallback } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { FONT_SIZES, FONT_WEIGHTS } from '@/utils/constants';
 import { COLORS } from '@/utils/constants/colors';
@@ -10,92 +10,73 @@ import AchivementDetailsModal from '../modals/AchivementDetailsModal';
 import { useAchievementsStore } from '@/store/achivement-store';
 import { Achievement } from '@/types';
 
-
-  const debounce = (func: Function, wait: number) => {
-    let timeout: NodeJS.Timeout;
-    return function executedFunction(...args: any[]) {
-      const later = () => {
-        clearTimeout(timeout);
-        func(...args);
-      };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-    };
-  };
- function AchivementListComponent() {
-
-    const {achievements, selectAchivement, isAchievementUnlocked} = useAchievementsStore()
-    const user = useAuthStore((state) => state.user);
-if (!user) return null;
-
-          const debouncedSelect = useCallback(
-            debounce((achievement: Achievement) => {
-              selectAchivement(achievement);
-            }, 300),
-            [selectAchivement],
-          );
+function AchivementListComponent() {
+  const { achievements, selectAchivement, isAchievementUnlocked } =
+    useAchievementsStore();
+  const user = useAuthStore((state) => state.user);
+  if (!user) return null;
 
   return (
     <View>
-              <ScrollView
-          showsHorizontalScrollIndicator={true}
-          horizontal
-          style={styles.horizontalScroll}
-        >
-          {achievements.length > 0 ? (
-            achievements.map((achievement) => {
-              return (
-                <AchievementCard
-                  key={achievement.id}
-                  achievement={achievement}
-                  status={isAchievementUnlocked(achievement.id)}
-                onPress={()=>debouncedSelect(achievement)}
-                />
-              );
-            })
-          ) : (
-         				<LinearGradient
-					colors={["#FFFFFF", "#F0F4FF"]}
-					style={styles.emptyState}
-				>
-					<Text style={styles.emptyStateText}>
-						Oops..Achivements not loaded try again later.
-					</Text>
-				</LinearGradient>
-          )}
-        </ScrollView>
+      <ScrollView
+        showsHorizontalScrollIndicator={true}
+        horizontal
+        style={styles.horizontalScroll}
+      >
+        {achievements.length > 0 ? (
+          achievements.map((achievement) => {
+            return (
+              <AchievementCard
+                key={achievement.id}
+                achievement={achievement}
+                status={isAchievementUnlocked(achievement.id)}
+                onPress={() => selectAchivement(achievement)}
+              />
+            );
+          })
+        ) : (
+          <LinearGradient
+            colors={['#FFFFFF', '#F0F4FF']}
+            style={styles.emptyState}
+          >
+            <Text style={styles.emptyStateText}>
+              Oops..Achivements not loaded try again later.
+            </Text>
+          </LinearGradient>
+        )}
+      </ScrollView>
 
-        <AchivementDetailsModal/>
+      <AchivementDetailsModal />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   horizontalScroll: {
     marginBottom: 24,
   },
-      	emptyState: {
-		borderRadius: 24,
-		padding: 24,
-		alignItems: "center",
-		justifyContent: "center",
-		marginHorizontal: 16,
-		marginBottom: 24,
-		elevation: 4,
-		shadowColor: "#000",
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.1,
-		shadowRadius: 4,
-		borderWidth: 2,
-		borderColor: "rgba(255, 255, 255, 0.6)",
-	},
-	emptyStateText: {
-		fontSize: FONT_SIZES.md,
-		color: COLORS.textLight,
-		textAlign: "center",
-		fontWeight: FONT_WEIGHTS.bold,
-	},
-})
+  emptyState: {
+    borderRadius: 24,
+    padding: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 16,
+    marginBottom: 24,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    borderWidth: 2,
+    borderColor: 'rgba(255, 255, 255, 0.6)',
+  },
+  emptyStateText: {
+    fontSize: FONT_SIZES.md,
+    color: COLORS.textLight,
+    textAlign: 'center',
+    fontWeight: FONT_WEIGHTS.bold,
+  },
+});
 
 const AchivementList = React.memo(AchivementListComponent);
 
